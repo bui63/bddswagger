c24d8a9cb99fa5dcad9f1dc1d9c6ddd8b72772ec
Rahul Kumar Singh <singhrahulkr1002@gmail.com>
2020-04-27 09:33:20 +0530

    added lombok dependency

--- src/main/java/com/demo/swagger/users/Interf.java
+++
@@ -1,8 +0,0 @@
-/*
-package com.demo.swagger.users;
-@FunctionalInterface
-public interface Interf {
-    public void m();
-
-}
-*/
--- src/main/java/com/demo/swagger/users/UserController.java
+++ src/main/java/com/demo/swagger/users/UserController.java
@@ -1,10 +1,6 @@
 package com.demo.swagger.users;
 
 
-import java.net.URI;
-import java.util.List;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -12,37 +8,40 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
+import java.util.List;
+
 
 @RestController
 public class UserController {
 
     @Autowired
     private UserDaoService service;
+
     //GET /users
     // retrieveAllUsers
     @GetMapping("/users")
-    public ResponseEntity<List<User>> retrieveAllUsers()
-    {
+    public ResponseEntity<List<User>> retrieveAllUsers() {
 
-        List <User> userList = service.findALL();
+        List<User> userList = service.findALL();
 
-        return new ResponseEntity<List<User>> (userList,new HttpHeaders(),HttpStatus.OK);
+        return new ResponseEntity<List<User>>(userList, new HttpHeaders(), HttpStatus.OK);
     }
 
     //GET /user/{id}
     //retrieveUser
     @GetMapping("/users/{id}")
     public ResponseEntity<User> retrieveUser(@PathVariable int id) {
-        User user =service.findOne(id);
-        return new ResponseEntity<User>(user,new HttpHeaders(),HttpStatus.OK);
+        User user = service.findOne(id);
+        return new ResponseEntity<User>(user, new HttpHeaders(), HttpStatus.OK);
     }
-    
-    @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user){
-        User savedUser=service.save(user);
-        URI location=ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
 
-        return new ResponseEntity<>(savedUser, new HttpHeaders(),HttpStatus.CREATED);
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+
+        return new ResponseEntity<>(savedUser, new HttpHeaders(), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/users/{id}")
